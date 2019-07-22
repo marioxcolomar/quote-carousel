97490b23f321a8ebb7d9760053c76e508212fd6a
mariocoloma <mario@hairpal.dk>
2019-07-22 03:15:31 +0200

    Rename and place in new folder > App sources.

---
+++ src/components/App/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import QuoteSlider from '../QuoteSlider';
+
+function App() {
+  return (
+    <div className="App">
+      <link rel='stylesheet' href='https://use.typekit.net/izy8aoj.css' />
+      <QuoteSlider />
+    </div>
+  );
+}
+
+export default App;
