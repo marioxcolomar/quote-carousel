b6767386b7bfa95746df4f374d90ea4f0dafb7b2
mariocoloma <mario@hairpal.dk>
2019-07-22 03:17:05 +0200

    Add array of quotes

---
+++ src/quotes.js
@@ -0,0 +1,32 @@
+export const quotes = [
+  {
+    id: 1,
+    text: "This piece of software is the shit, I've never tried anythinh like it",
+    name: 'Mr. Face',
+    company: 'Karma Guru @ Face Co',
+  },
+  {
+    id: 2,
+    text: "An experience that makes us feel like we're covered in maple syrup - it's a good thing",
+    name: 'The whole team @',
+    company: 'The Pancake Collective',
+  },
+  {
+    id: 3,
+    text: 'Great carousel, great guy!',
+    name: 'Me @',
+    company: 'Me Company',
+  },
+  {
+    id: 4,
+    text: 'Curabitizzle owned dizzle quizzle nisi things mollizzle. Fo shizzle my nizzle crocodizzle.',
+    name: 'Wasted talent @',
+    company: 'Fo Shizzle Inc',
+  },
+  {
+    id: 5,
+    text: 'Asymmetrical cold-pressed la croix, tousled cliche distillery subway tile marfa celiac.',
+    name: 'Humblebrag artisan @',
+    company: 'Meggings Street Art',
+  },
+]
