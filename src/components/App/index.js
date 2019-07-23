545e34de1857a6525a6b05deff98f13ac7d1ea65
mariocoloma <mario@hairpal.dk>
2019-07-23 10:59:40 +0200

    Use helmet in App component.

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,11 +1,16 @@
 import React from 'react';
+import { Helmet } from 'react-helmet'
 import './App.css';
 import QuoteSlider from '../QuoteSlider';
 
 function App() {
   return (
     <div className="App">
-      <link rel='stylesheet' href='https://use.typekit.net/izy8aoj.css' />
+      <Helmet>
+        <meta charSet="UTF-8" />
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" />
+        <link rel='stylesheet' href='https://use.typekit.net/izy8aoj.css' />
+      </Helmet>
       <QuoteSlider />
     </div>
   );
