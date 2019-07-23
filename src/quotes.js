db9d3db89c4d0a39f06a437b626c95ecf6a696e0
mariocoloma <mario@hairpal.dk>
2019-07-23 10:59:17 +0200

    Import icons to quotes.js

--- src/quotes.js
+++ src/quotes.js
@@ -1,32 +1,40 @@
+import faceIcon from './assets/face-icon.svg'
+import cakeIcon from './assets/cake-icon.svg'
+
 export const quotes = [
   {
     id: 1,
     text: "This piece of software is the shit, I've never tried anythinh like it",
     name: 'Mr. Face',
     company: 'Karma Guru @ Face Co',
+    icon: faceIcon,
   },
   {
     id: 2,
     text: "An experience that makes us feel like we're covered in maple syrup - it's a good thing",
     name: 'The whole team @',
     company: 'The Pancake Collective',
+    icon: cakeIcon,
   },
   {
     id: 3,
     text: 'Great carousel, great guy!',
     name: 'Me @',
     company: 'Me Company',
+    icon: faceIcon,
   },
   {
     id: 4,
     text: 'Curabitizzle owned dizzle quizzle nisi things mollizzle. Fo shizzle my nizzle crocodizzle.',
     name: 'Wasted talent @',
     company: 'Fo Shizzle Inc',
+    icon: cakeIcon,
   },
   {
     id: 5,
     text: 'Asymmetrical cold-pressed la croix, tousled cliche distillery subway tile marfa celiac.',
     name: 'Humblebrag artisan @',
     company: 'Meggings Street Art',
+    icon: faceIcon,
   },
 ]
